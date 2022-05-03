00df69885f680691774cce04ceae9a9719f569ad
Samreensaba <Samren_saba@yahoo.com>
2022-05-03 15:52:37 +0100

    Create PartTwo.js

---
+++ PartTwo.js
@@ -0,0 +1,88 @@
+// Additional assignments for Day 3
+
+/* EXTRA 1
+ Use a ternary operator to assign to a variable called gender the string values "male" or "female".
+ The choice should be made based on the value of another variable called isMale.
+*/
+
+/* var gender = male? "Male" : "Female"
+console.log("Gender is" gender) */
+
+/* EXTRA 2
+ Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 3
+ Create a variable and assign to it the concatenation of two strings.
+*/
+
+/* let name1 = "John"
+let name2 = "Bosco"
+let FullName = name1+nam2
+console.log(FullName) */
+
+/* EXTRA 4
+ Create three variables and assign a numerical value to each one of them. 
+ Using a conditional statement, write a piece of code for sorting their values from highest to lowest.
+ Display the result in the console.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 5
+ Write a piece of code for finding the average of two given integers.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 6
+ Write a piece of code for finding the longest of two given strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 7
+ Write a piece of code for checking if a given value is a integer or not.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 8
+ Write a piece of code for calculating a certain percentage of a given number.
+ (Ex.: the 20% of 400 is 80)
+*/
+
+/* Console.log(" The 20% of 400 is", 400 % 20) */
+
+/* EXTRA 9
+ Write a piece of code for checking if a given number is even or odd.
+*/
+
+/* let x = 3
+if (X%2 === 0){
+    console.log("The number is Even");
+}
+else{
+    console.log("The number is Odd")
+}
+*/
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
+var gender = male? "male" : "female";
+console.log("Gender is", gender)
+let name1 = "John"
+let name2 = "Bosco"
+let FullName = name1+nam2
+console.log(FullName)
+Console.log(" The 20% of 400 is", 400 % 20)
+let x = 3
+if (X%2 === 0){
+    console.log("The number is Even");
+}
+else{
+    console.log("The number is Odd")
+}
