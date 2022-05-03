6b53b1355e5931afa7616229fa2dd7fb5b355d06
Samreensaba <Samren_saba@yahoo.com>
2022-05-03 14:12:42 +0100

    Create Day7.js

---
+++ Day7.js
@@ -0,0 +1,23 @@
+console.log(12 + 20)
+let x = 12
+console.log(x)
+let name = "John Doe"
+console.log(name)
+console.log(12 - x)
+let name1
+let name2
+name1= "john"
+name2= "John"
+console.log("Is name1 and name2 same?", name1 === name2)
+console.log("Is name1 and name2 same?", name1 === name2.toLowerCase())
+let Age = 30
+let ModelElg
+if (Age >= 30){
+ModelElg = "not eligible";
+}
+else{
+    ModelElg = "eligible";
+}
+console.log("********Ternary Operator********")
+let ModelEligibility = (Age <30)? "Eligible" : "not eligible";
+console.log(ModelEligibility)
